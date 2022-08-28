e05f69715afa648d5218487ecc7c981a15ea6fc6
howz97 <zhanghao997@qq.com>
2022-08-28 11:40:24 +0800

    seq scan

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,24 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan)
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      itr_(exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid())->table_->Begin(exec_ctx_->GetTransaction())) {}
 
 void SeqScanExecutor::Init() {}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  TableInfo *tbl_info = exec_ctx_->GetCatalog()->GetTable(plan_->GetTableOid());
+  for (; itr_ != tbl_info->table_->End(); ++itr_) {
+    *tuple = *itr_;
+    *rid = itr_->GetRid();
+    if (plan_->GetPredicate()->Evaluate(tuple, &tbl_info->schema_).GetAs<bool>()) {
+      ++itr_;
+      return true;
+    };
+  }
+  return false;
+}
 
 }  // namespace bustub
